4bd21a476d58a5518f104b7d89dc3c70c3c023f3
Anthony Pesch <anthony@usamp.com>
2012-10-01 20:42:50 -0700

    Split up re-world and re-sky

---
+++ js/renderer/re-sky.js
@@ -0,0 +1,63 @@
+var q3render_sky_vertex_stride = 20;
+
+var skyboxMat = mat4.create();
+var skyboxBuffer = null;
+var skyboxIndexBuffer = null;
+var skyboxIndexCount;
+var skyShader = null;
+
+function BuildSkyboxBuffers() {
+	var skyVerts = [
+		-128, 128, 128, 0, 0,
+		128, 128, 128, 1, 0,
+		-128, -128, 128, 0, 1,
+		128, -128, 128, 1, 1,
+
+		-128, 128, 128, 0, 1,
+		128, 128, 128, 1, 1,
+		-128, 128, -128, 0, 0,
+		128, 128, -128, 1, 0,
+
+		-128, -128, 128, 0, 0,
+		128, -128, 128, 1, 0,
+		-128, -128, -128, 0, 1,
+		128, -128, -128, 1, 1,
+
+		128, 128, 128, 0, 0,
+		128, -128, 128, 0, 1,
+		128, 128, -128, 1, 0,
+		128, -128, -128, 1, 1,
+
+		-128, 128, 128, 1, 0,
+		-128, -128, 128, 1, 1,
+		-128, 128, -128, 0, 0,
+		-128, -128, -128, 0, 1
+	];
+
+	var skyIndices = [
+		0, 1, 2,
+		1, 2, 3,
+
+		4, 5, 6,
+		5, 6, 7,
+
+		8, 9, 10,
+		9, 10, 11,
+
+		12, 13, 14,
+		13, 14, 15,
+
+		16, 17, 18,
+		17, 18, 19
+	];
+
+	skyboxBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, skyboxBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(skyVerts), gl.STATIC_DRAW);
+
+	skyboxIndexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, skyboxIndexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(skyIndices), gl.STATIC_DRAW);
+
+	skyboxIndexCount = skyIndices.length;
+}
