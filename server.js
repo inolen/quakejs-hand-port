dbc22aa59d0830546c9f7f075705a6a1c73c0bcf
Anthony Pesch <anthony@usamp.com>
2012-11-24 22:47:47 -0800

    Removed EJS dependency

--- server.js
+++ server.js
@@ -1,35 +1,58 @@
 var express = require('express'),
+	fs = require('fs'),
 	http = require('http'),
 	path = require('path');
+
+var arguments = process.argv.splice(2);
+
+// Used by the build process.
+if (arguments.length == 2 && arguments[0] === '--processScript') {
+	var output = processScript(arguments[1]);
+	console.log(output);
+	return;
+}
+
+function processScript(scriptname) {
+	var scriptdir = path.dirname(scriptname);
+	var text = fs.readFileSync(scriptname, 'utf8');
+
+	// Replace includes.
+	text = text.replace(/(["'\{]{2,})\s+include\s+(.+?)\s+(["'\}]{2,})/g, function(match, open, filename, close) {
+		// Process nested includes.
+		var contents = processScript(scriptdir + '/' + filename);
+
+		// Check to see if the include is being used as a string.
+		if ((open.charAt(0) === "'" && close.charAt(close.length-1) === "'") || 
+			(open.charAt(0) === '"' && close.charAt(close.length-1) === '"')) {
+			// If so, replace newlines and escape quotation marks.
+			contents.replace(/[\r\n]+/g, '').replace(/'/g, "\\'");
+			contents = "'" + contents + "'";
+		}
+
+		return contents;
+	});
+
+	return text;
+}
+
+function expressScript(req, res, next) {
+	var scriptname = path.normalize(__dirname + req.url);
+	var text = processScript(scriptname);
+	res.send(text);
+}
 
 function main() {
 	var app = express();
 	var server = http.createServer(app);
-	var ejs = require('ejs');
 
-	ejs.open = '{{';
-	ejs.close = '}}';
-
-	app.engine('ejs', ejs.__express);
 	app.use(express.compress({
 		filter: function(req, res){
 			return /json|text|javascript|octet-stream/.test(res.getHeader('Content-Type'));
 		}
 	}));
 	app.use(express.static(__dirname + '/public', { maxAge: 86400000 }));
-	app.use('/lib', express.static(__dirname + '/lib'));
 	app.use('/bin', express.static(__dirname + '/bin'));
-
-	// Our build process will pre-process these .ejs views,
-	// but if they don't exist we need to build them at runtime.
-	app.get('*.js', function (req, res, next) {
-		var file = __dirname + req.params[0] + '.js.ejs';
-
-		path.exists(file, function(exists) {
-			if (!exists) return next();
-			return res.render(file);
-		});
-	});
+	app.get('*.js', expressScript);
 
 	server.listen(9000);
 	
