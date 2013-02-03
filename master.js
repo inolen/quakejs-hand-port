7921bba583d4b7b454e5d9269707e0d9bd3d4f77
Anthony Pesch <anthony@usamp.com>
2013-02-03 01:22:28 -0800

    Converted from WebSocket to ws for node.js websockets, first pass at master server

--- master.js
+++ master.js
@@ -1,52 +1,113 @@
 var _ = require('underscore');
+var express = require('express');
 var http = require('http');
-var WebSocketServer = require('websocket').server;
+var url = require('url');
+var WebSocketClient = require('ws');
+
+var argv = require('optimist')
+	.default('port', 45735)
+	.argv;
+
+var servers = [];
+var scanInterval = 30 * 1000;
 
 function main() {
-	var cfg = loadConfig();
-	launchServer(cfg.port);
+	createServer(argv.port);
+
+	setTimeout(scanServers, scanInterval);
 }
 
-function loadConfig() {
-	var cfg = {
-		port: 9003
-	};
-	try {
-		var data = require('./master.json');
-		_.extend(cfg, data);
-	} catch (e) {
-	}
+function createServer(port) {
+	var app = express();
 
-	return cfg;
-}
+	app.use(express.compress());
+	app.use(express.bodyParser());
 
-function launchServer(port) {
-	var server = http.createServer();
-	var wsServer = new WebSocketServer({
-		httpServer: server,
-		autoAcceptConnections: false
-	});
+	var server = http.createServer(app);
 
-	wsServer.on('request', function(request) {
-		var connection = request.accept('quakejs', request.origin);
-		console.log((new Date()), 'Connection accepted.');
+	app.post('/heartbeat', handleHeartbeat);
+	app.get('/servers', handleServers);
 
-		connection.on('message', function (message) {
-			console.log('GOT A MESSAGE');
-			var data = message.binaryData;
-			var ab = new ArrayBuffer(data.length);
-			var view = new Uint8Array(ab);
-			for (var i = 0; i < data.length; ++i) {
-				view[i] = data[i];
-			}
-
-			console.log(ab);
-		});
-	});
-
-	server.listen(port, function() {
+	server.listen(port, function () {
 		console.log('Master server is now listening on port', port);
 	});
+
+	return server;
 }
 
-main();
+function handleHeartbeat(req, res, next) {
+	var hostname = req.connection.remoteAddress.toLowerCase();
+	var port = parseInt(req.body.port, 10);
+
+	res.type('json');
+
+	if (isNaN(port)) {
+		res.send(500, { error: 'Invalid port number.' });
+		return;
+	}
+
+	var address = hostname + ':' + port;
+
+	console.log('Received heartbeat from', address);
+
+	if (servers.indexOf(address) === -1) {
+		servers.push(address);
+	}
+
+	res.send({ message: 'success' });
+	res.end();
+}
+
+function handleServers(req, res, next) {
+	res.type('json');
+	res.send(servers);
+}
+
+function scanServers() {
+
+}
+
+function scanServer(callback) {
+	var ws = new WebSocketClient('ws://localhost:9001');
+
+	ws.on('open', function () {
+		console.log('WebSocket client connected');
+
+		// FIXME node.js encodes 0x0 as 0x20 for some reason so we force it.
+		// https://github.com/joyent/node/issues/297
+		var buff = new Buffer('\xff\xff\xff\xffgetinfo\0', 'ascii');
+		buff[buff.length - 1] = 0;
+
+		ws.send(buff, { binary: true });
+	});
+
+	ws.on('message', function (data, flags) {
+		if (!flags.binary) {
+			return callback(new Error('Received non-binary response.'));
+		}
+
+		if (data.readInt8(0) !== -1) {
+			return callback(new Error('Invalid header.'));
+		}
+
+		// Account for null-terminator.
+		console.log(data.length);
+
+		var str = data.toString('ascii', 4, data.length - 1);
+		var json = JSON.parse(str);
+
+		ws.close();
+
+		return callback(null, json);
+	});
+
+	ws.on('error', function (err) {
+		return callback(err);
+	});
+}
+
+scanServer(function (err, json) {
+	console.log(err, json);
+});
+
+// main();
