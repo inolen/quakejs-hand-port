51e4b2d548b90878d4eefd7ec015cf1dc34e709d
Anthony Pesch <anthony@usamp.com>
2013-02-09 14:11:25 -0800

    Moved to custom knockout build

--- build/browser.build.js
+++ build/browser.build.js
@@ -6,6 +6,7 @@
 		'gameshim':     'vendor/game-shim',
 		'glmatrix':     'vendor/gl-matrix',
 		'jquery':       'vendor/jquery-1.8.2',
-		'underscore':   'vendor/underscore',
+		'knockout':     'vendor/knockout-latest',
+		'underscore':   'vendor/underscore'
 	},
 })
