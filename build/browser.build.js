b3508cbe36eee3d77474174ac2a4e80db226f3bf
Anthony Pesch <anthony@usamp.com>
2013-02-17 16:03:48 -0800

    Updated to commonjs format

--- build/browser.build.js
+++ build/browser.build.js
@@ -5,7 +5,6 @@
 		'EventEmitter': 'vendor/EventEmitter',
 		'gameshim':     'vendor/game-shim',
 		'glmatrix':     'vendor/gl-matrix',
-		'include':      'vendor/requirejs-include',
 		'jquery':       'vendor/jquery-1.8.2',
 		'knockout':     'vendor/knockout-latest',
 		'text':         'vendor/text',
