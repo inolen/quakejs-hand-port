77ec2cf37a813d0cb4550ecc7796fade2fa614e4
Anthony Pesch <anthony@usamp.com>
2013-02-19 23:30:17 -0800

    Removed default.cfg and moved filecdn back to cvar system

--- dedicated.js
+++ dedicated.js
@@ -1,16 +1,4 @@
-var opt = require('optimist');
 var requirejs = require('./build/r.js');
-var url = require('url');
-
-var argv = opt
-	.usage('Launch a dedicated quakejs server.\nUsage: $0')
-	.describe('filecdn', 'CDN root for files').default('filecdn', 'http://content.quakejs.com')
-	.argv;
-
-if (argv.h || argv.help) {
-	opt.showHelp();
-	return;
-}
 
 function main() {
 	requirejs.config({
@@ -22,7 +10,7 @@ function main() {
 	});
 
 	requirejs(['system/dedicated/sys'], function (sys) {
-		sys.Init(argv.filecdn);
+		sys.Init();
 	});
 }
 
